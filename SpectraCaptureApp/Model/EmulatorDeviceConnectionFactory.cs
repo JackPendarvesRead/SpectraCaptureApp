7eba88678b6999767dd56ea1ade604458666be74
JackRead <jackaunir@gmail.com>
2020-03-23 10:12:17 +0000

    Uncomment

--- SpectraCaptureApp/Model/EmulatorDeviceConnectionFactory.cs
+++ SpectraCaptureApp/Model/EmulatorDeviceConnectionFactory.cs
@@ -1,19 +1,19 @@
-﻿//using Aunir.InstrumentControl.Interfaces;
-//using Aunir.InstrumentControl.Jdsu.Emulator;
-//using NIR4.ViaviCapture.Model;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Aunir.InstrumentControl.Interfaces;
+using Aunir.InstrumentControl.Jdsu.Emulator;
+using NIR4.ViaviCapture.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SpectraCaptureApp.Model
-//{
-//    public class EmulatorDeviceConnectionFactory : INirDeviceConnectionFactory
-//    {
-//        public INirDeviceConnection GetConnection()
-//        {
-//            return new JdsuEmulatorConnection();
-//        }
-//    }
-//}
+namespace SpectraCaptureApp.Model
+{
+    public class EmulatorDeviceConnectionFactory : INirDeviceConnectionFactory
+    {
+        public INirDeviceConnection GetConnection()
+        {
+            return new JdsuEmulatorConnection();
+        }
+    }
+}
