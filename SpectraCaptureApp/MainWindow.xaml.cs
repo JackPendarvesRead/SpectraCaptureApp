b4a7e942abb0ca05954198dcc6f27270e9894369
JackRead <jackaunir@gmail.com>
2020-05-29 16:27:44 +0100

    changed main window to be just the router host

--- SpectraCaptureApp/MainWindow.xaml.cs
+++ SpectraCaptureApp/MainWindow.xaml.cs
@@ -13,8 +13,6 @@ namespace SpectraCaptureApp
             this.WhenActivated(disposables =>
             {
                 this.OneWayBind(ViewModel, vm => vm.Router, view => view.RoutedViewHost.Router).DisposeWith(disposables);
-                this.BindCommand(ViewModel, vm => vm.SettingsNavigateCommand, view => view.SettingsButton).DisposeWith(disposables);
-                this.BindCommand(ViewModel, vm => vm.NewScan, view => view.RestartButton).DisposeWith(disposables);
             });
         }
     }
