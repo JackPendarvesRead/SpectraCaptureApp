db391a127804d480cb458863f9a38b47261caf42
JackRead <jackaunir@gmail.com>
2020-05-29 17:32:51 +0100

    Added a load of navbar stuff but giving up until symantec stops messing with me

---
+++ SpectraCaptureApp/View/Controls/TopBar.xaml.cs
@@ -0,0 +1,44 @@
+﻿using SpectraCaptureApp.ViewModel;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SpectraCaptureApp.ViewModel.Controls;
+
+namespace SpectraCaptureApp.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for FileBrowse.xaml
+    /// </summary>
+    public partial class TopBar : ReactiveUserControl<TopBarViewModel>
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+            this.WhenActivated(disposables =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.BatteryImage, view => view.BatteryImage.Source).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.SpectrometerConnectedImageUri, view => view.SpectrometerConnectedImage.Source).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.BaselineOkImageUri, view => view.BaselineOkImage.Source).DisposeWith(disposables);
+
+                this.OneWayBind(ViewModel, vm => vm.HomeButtonVisible, view => view.HomeButton.Visibility).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.SettingsButtonVisible, view => view.SettingsButton.Visibility).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.BaselineOkImageVisible, view => view.BaselineOkImage.Visibility).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.BatteryImageVisible, view => view.BatteryImage.Visibility).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.SpectrometerConnectedImageVisible, view => view.SpectrometerConnectedImage.Visibility).DisposeWith(disposables);
+            });
+        }
+    }
+}
---
+++ SpectraCaptureApp/ViewModel/Controls/TopBarViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SpectraCaptureApp.ViewModel.Controls
+{
+    public class TopBarViewModel : ReactiveObject
+    {
+        public ImageSource BaselineOkImageUri => new BitmapImage(new Uri("/Images/tick.png"));
+        public ImageSource SpectrometerConnectedImageUri => new BitmapImage(new Uri("/Images/tick.png"));
+        public ImageSource BatteryImage => new BitmapImage(new Uri("/Images/100.png"));
+
+        public Visibility HomeButtonVisible { get; set; }
+        public Visibility SettingsButtonVisible { get; set; }
+        public Visibility BatteryImageVisible { get; set; }
+        public Visibility BaselineOkImageVisible { get; set; }
+        public Visibility SpectrometerConnectedImageVisible { get; set; }
+
+        public ReactiveCommand<Unit, IRoutableViewModel> SettingsNavigateCommand { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> HomeNavigateCommand { get; }
+
+        public TopBarViewModel()
+        {
+            HomeButtonVisible = Visibility.Collapsed;
+            SettingsButtonVisible = Visibility.Visible;
+            BatteryImageVisible = Visibility.Visible;
+            BaselineOkImageVisible = Visibility.Visible;
+            SpectrometerConnectedImageVisible = Visibility.Visible;
+        }
+    }
+}
