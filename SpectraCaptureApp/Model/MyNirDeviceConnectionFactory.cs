4e744321c03c79b25127ddd292d03864369e65d2
JackRead <jackaunir@gmail.com>
2020-07-30 15:37:48 +0100

    Testing

--- SpectraCaptureApp/Model/MyNirDeviceConnectionFactory.cs
+++ SpectraCaptureApp/Model/MyNirDeviceConnectionFactory.cs
@@ -9,24 +9,50 @@ namespace SpectraCaptureApp.Model
 {
     public class MyNirDeviceConnectionFactory : INirDeviceConnectionFactory
     {
-        private readonly JdsuDeviceController controller;
-
-        public MyNirDeviceConnectionFactory()
-        {
-            controller = new JdsuDeviceController();
-        }
+        private readonly JdsuDeviceController controller = new JdsuDeviceController();
+        private static INirDeviceConnection connection;
+        private static string ConnectedDeviceSerial;
 
         public INirDeviceConnection GetConnection()
         {
-            var devices = controller.GetConnectedDevices();
+            if(connection == null)
+            {
+                SetConnection();
+            }
+            else
+            {
+                var connected = CheckDeviceIsConnected();
+                if (!connected)
+                {
+                    connection = null;
+                    SetConnection();
+                }
+            }
+            return connection;
+        }
 
+        private bool CheckDeviceIsConnected()
+        {
+            var serial = GetDeviceSerial();
+            return serial == ConnectedDeviceSerial;
+        }
+
+        private void SetConnection()
+        {
+            ConnectedDeviceSerial = GetDeviceSerial();
+            connection = controller.CreateConnection(ConnectedDeviceSerial);
+        }
+
+        private string GetDeviceSerial()
+        {
+            var devices = controller.GetConnectedDevices();
             if (devices.Length < 1)
                 throw new HardwareException("No Devices Found");
 
             if (devices.Length > 1)
                 throw new HardwareException("More than one device found");
 
-            return controller.CreateConnection(devices[0]);
+            return devices[0];
         }
     }
 }
